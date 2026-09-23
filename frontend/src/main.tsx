4117d744778236ad1f64a28f6cd42c0b0836d4fa
Keikei-p <313776214+Keikei-p@users.noreply.github.com>
2026-09-23 12:35:36 +0900

    feat: gate cloud mode behind login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { AuthGate } from "./AuthGate";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AuthGate>
+      {({ email, signOut }) => <App cloudUserEmail={email} onCloudSignOut={signOut} />}
+    </AuthGate>
   </StrictMode>
 );
